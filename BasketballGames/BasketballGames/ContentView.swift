1aac0a80c1d126dc740af78f4381f5565f07826a
Davids122 <davidsprague122@gmail.com>
2025-03-03 11:07:51 -0500

    finish both

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,15 +7,66 @@
 
 import SwiftUI
 
+struct Game: Codable {
+    var isHomeGame: Bool
+    var team: String
+    var date: String
+    var opponent: String
+    var id: Int
+    var score: Score
+}
+
+struct Score: Codable {
+    var unc: Int
+    var opponent: Int
+}
+
 struct ContentView: View {
+    @State var games: [Game] = [Game]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(games, id: \.id) { game in
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("\(game.team) vs \(game.opponent)")
+                        
+                        Text(game.date)
+                            .foregroundStyle(.secondary)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(alignment: .leading) {
+                        Text(String("\(game.score.unc)  - \(game.score.opponent)"))
+                        
+                        Text(game.isHomeGame ? "Home" : "Away")
+                            .foregroundStyle(.secondary)
+                    }
+                }
+            }.task {
+                await loadData()
+            }
+            .navigationTitle("UNC Basketball")
         }
-        .padding()
+    }
+    
+    func loadData() async {
+        guard let url = URL(
+            string: "https://api.samuelshi.com/uncbasketball"
+        ) else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            if let decodedGames = try? JSONDecoder().decode([Game].self, from: data) {
+                games = decodedGames
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
